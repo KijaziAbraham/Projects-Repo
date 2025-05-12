9cc5a66958c27c93e3f9c3a0a88cafa02bf1c60c
DICKSON78 <augustinodickson78@gmail.com>
2025-05-12 13:36:00 +0300

    Code reviewed clearly

--- REACTNMU/prototype-management/src/components/Navbar.js
+++ REACTNMU/prototype-management/src/components/Navbar.js
@@ -1,21 +1,15 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { BsPlusLg } from "react-icons/bs";
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const DashboardNavbar = ({ user, searchTerm, setSearchTerm, handleShowSubmitModal, userRole }) => {
-  const navigate = useNavigate();
-  
   return (
     <Navbar expand="lg" className="bg-body-light mb-2" style={{borderBottom:"2px solid gray"}}>
       <Container fluid>
-      <Navbar.Brand className="text text-bg" style={{fontSize:'1.5rem',color:'#64A293'}}>
-        Logged in as :  {user ? `${user.full_name}` : ""}
+        <Navbar.Brand className="text text-bg" style={{fontSize:'1.5rem',color:'#64A293'}}>
+          Logged in as :  {user ? `${user.full_name}` : ""}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
@@ -23,18 +17,17 @@ const DashboardNavbar = ({ user, searchTerm, setSearchTerm, handleShowSubmitModa
             {/* You can add navigation items here if needed */}
           </Nav>
           <div className="controls-row">
-              <div class="search-control">
-                <input
-              type="search"
-              placeholder="Search prototypes..."
-              className="me-2"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
+            <div className="search-control">
+              <input
+                type="search"
+                placeholder="Search prototypes..."
+                className="me-2"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
             </div>
-            <Button style={{backgroundColor:'#64A293',border:'none',}}>Search</Button>
-         
-</div>
+            <Button style={{backgroundColor:'#64A293',border:'none'}}>Search</Button>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- REACTNMU/prototype-management/src/components/ProfileNavbar.js
+++ REACTNMU/prototype-management/src/components/ProfileNavbar.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { BsPlusLg } from "react-icons/bs";
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const DashboardNavbar = ({ user, searchTerm, setSearchTerm, handleShowSubmitModal, userRole }) => {
-  const navigate = useNavigate();
-  
   return (
     <Navbar expand="lg" className="bg-body-light mb-2" style={{borderBottom:"2px solid gray"}}>
       <Container fluid>
         <Navbar.Brand className="text text-bg" style={{fontSize:'1.5rem',color:'#64A293'}}>
-        Logged in as :  {user ? `${user.full_name}` : ""}
+          Logged in as :  {user ? `${user.full_name}` : ""}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
@@ -39,4 +34,4 @@ const DashboardNavbar = ({ user, searchTerm, setSearchTerm, handleShowSubmitModa
   );
 };
 
-export default DashboardNavbar;
+export default DashboardNavbar
